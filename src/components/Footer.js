cf2adb14610141aaf77915969109bd43d6535344
Sonia Alice George <sonia.sis94@gmail.com>
2021-09-06 18:57:20 -0400

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer className="absolute inset-x-0 bottom-0 bg-gray-300 h-20 flex flex-wrap content-center justify-center text-sm">
+            <p>&copy; Copyright 2021 | React Project</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header className="bg-black text-white shadow-md h-20 font-bold text-xl flex flex-wrap content-center justify-center">
+            <img src="../logo512.png" alt="logo" className="h-10 pr-5"/>
+            <h1>Welcome to my React Project</h1>
+        </header>
+    )
+}
+
+export default Header
